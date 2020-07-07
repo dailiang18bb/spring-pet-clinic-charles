674e0969f641a1e63c0b4232e451d73fff91e9f8
Charles <dailiang18bb@gmail.com>
2020-07-07 14:16:50 -0400

    Initial project add

---
+++ src/main/java/charles/spring/springpetcliniccharles/SpringPetClinicCharlesApplication.java
@@ -0,0 +1,13 @@
+package charles.spring.springpetcliniccharles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicCharlesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicCharlesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/charles/spring/springpetcliniccharles/SpringPetClinicCharlesApplicationTests.java
@@ -0,0 +1,13 @@
+package charles.spring.springpetcliniccharles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicCharlesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
