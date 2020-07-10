ac2ac1a5b4db74ffb1bf02dc3778c1acf4c2f2c0
Charles <dailiang18bb@gmail.com>
2020-07-09 23:42:29 -0400

    Adding BaseEntity, it gives the Id property. Closes #10

--- pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/model/Person.java
+++ pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/model/Person.java
@@ -1,6 +1,6 @@
 package charles.spring.springpetcliniccharles.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/model/PetType.java
+++ pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/model/PetType.java
@@ -1,6 +1,6 @@
 package charles.spring.springpetcliniccharles.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
