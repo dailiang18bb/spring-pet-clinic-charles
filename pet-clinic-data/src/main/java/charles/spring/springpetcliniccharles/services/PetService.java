ecf8f57d012382bb084ff422b0e6a9aff172f72f
Charles <dailiang18bb@gmail.com>
2020-07-09 23:32:23 -0400

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/services/PetService.java
@@ -0,0 +1,12 @@
+package charles.spring.springpetcliniccharles.services;
+
+import charles.spring.springpetcliniccharles.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
