22277187fcad69f3b40224d5e18d234e17de56c0
Charles <dailiang18bb@gmail.com>
2020-07-15 17:11:29 -0400

    Added PetTypeService Interface and PetTypeServiceMap. Closes #29

---
+++ pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package charles.spring.springpetcliniccharles.services;
+
+import charles.spring.springpetcliniccharles.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
