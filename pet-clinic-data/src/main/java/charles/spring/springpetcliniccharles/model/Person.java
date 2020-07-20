3b4fcb1747bf4498788d5bd8833fdc7cfb9e755d
Charles <dailiang18bb@gmail.com>
2020-07-20 16:05:51 -0400

    Updated owner with JPA entity mappings. Closes # 36 Updated related entities or classes, Pet, PetType, Person

--- pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/model/Person.java
+++ pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/model/Person.java
@@ -1,9 +1,19 @@
 package charles.spring.springpetcliniccharles.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+
 
     public String getFirstName() {
         return firstName;
