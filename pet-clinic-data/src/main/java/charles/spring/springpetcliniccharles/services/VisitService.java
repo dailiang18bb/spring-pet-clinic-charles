6aad0011c1bd5d7cf035745b1ca4c06e523d72de
Charles <dailiang18bb@gmail.com>
2020-07-21 17:14:50 -0400

    Created VisitService interface and Visit Map Service impl. Closes #46

---
+++ pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/services/VisitService.java
@@ -0,0 +1,6 @@
+package charles.spring.springpetcliniccharles.services;
+
+import charles.spring.springpetcliniccharles.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
