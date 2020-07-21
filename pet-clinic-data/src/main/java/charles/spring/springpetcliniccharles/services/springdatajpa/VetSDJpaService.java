5aa0ad4b046251fb21a92a848b0591b3e6dff406
Charles <dailiang18bb@gmail.com>
2020-07-21 16:10:28 -0400

    Implemented Vet JPA Service. Closes #41 JPA Service is the alternative service of our own HashMap map Service. persist data using SQL => JPA Service Persist data using our own HashMap => own map Service

---
+++ pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,50 @@
+package charles.spring.springpetcliniccharles.services.springdatajpa;
+
+
+import charles.spring.springpetcliniccharles.model.Vet;
+import charles.spring.springpetcliniccharles.repositories.VetRepository;
+import charles.spring.springpetcliniccharles.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
