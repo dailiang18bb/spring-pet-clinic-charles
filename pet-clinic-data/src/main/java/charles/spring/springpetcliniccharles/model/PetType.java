9bc4aa82e8996e14cd372950884349bc6bdd5f2a
Charles <dailiang18bb@gmail.com>
2020-08-17 14:42:39 -0400

    Added project Lombok, closes #51

--- pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/model/PetType.java
+++ pet-clinic-data/src/main/java/charles/spring/springpetcliniccharles/model/PetType.java
@@ -1,23 +1,21 @@
 package charles.spring.springpetcliniccharles.model;
 
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
-
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
